c884ef3b9a0fdb30a53977aae25866fd76ce065a
Walter Moreira <walter@waltermoreira.net>
2022-06-13 17:31:54 -0500

    Add the implementation for fs_jobs

--- src/fs_job.rs
+++ src/fs_job.rs
@@ -8,20 +8,22 @@ use std::{
 use serde::{de::DeserializeOwned, Serialize};
 use uuid::Uuid;
 
-use crate::{Job, JobInfo};
+use crate::{Job, JobInfo, StatusType};
 
 /// A basic implementation of the trait [`Job`].
 ///
 /// This implementation saves the job metadata [`JobInfo`] in a file, using
 /// the job id to make the file unique.
 #[derive(Clone)]
-pub struct FSJob<Output, Error> {
+pub struct FSJob<Output, Error, Metadata, Status> {
     job_directory: PathBuf,
     output_type: PhantomData<Output>,
     error_type: PhantomData<Error>,
+    metadata_type: PhantomData<Metadata>,
+    status_type: PhantomData<Status>,
 }
 
-impl<Output, Error> FSJob<Output, Error> {
+impl<Output, Error, Metadata, Status> FSJob<Output, Error, Metadata, Status> {
     /// Create a new [`FSJob`].
     ///
     /// The argument indicates a directory where to save the files for each job.
@@ -30,6 +32,8 @@ impl<Output, Error> FSJob<Output, Error> {
             job_directory,
             output_type: PhantomData,
             error_type: PhantomData,
+            metadata_type: PhantomData,
+            status_type: PhantomData,
         }
     }
 }
@@ -37,14 +41,24 @@ impl<Output, Error> FSJob<Output, Error> {
 impl<
         Output: Clone + Send + Sync + Serialize + DeserializeOwned + 'static,
         Error: Clone + Send + Sync + Serialize + DeserializeOwned + 'static,
-    > Job for FSJob<Output, Error>
+        Metadata: Clone + Send + Sync + Serialize + DeserializeOwned + 'static,
+        Status: StatusType
+            + Clone
+            + Send
+            + Sync
+            + Serialize
+            + DeserializeOwned
+            + 'static,
+    > Job for FSJob<Output, Error, Metadata, Status>
 {
     type Output = Output;
     type Error = Error;
+    type Metadata = Metadata;
+    type Status = Status;
 
     fn save(
         &self,
-        info: &JobInfo<Self::Output, Self::Error>,
+        info: &JobInfo<Self::Output, Self::Error, Self::Metadata, Self::Status>,
     ) -> Result<(), std::io::Error> {
         let mut file =
             File::create(self.job_directory.join(info.id.to_string()))?;
@@ -55,11 +69,19 @@ impl<
     fn load(
         &self,
         id: Uuid,
-    ) -> Result<JobInfo<Self::Output, Self::Error>, std::io::Error> {
+    ) -> Result<
+        JobInfo<Self::Output, Self::Error, Self::Metadata, Self::Status>,
+        std::io::Error,
+    > {
         let mut file = File::open(self.job_directory.join(id.to_string()))?;
         let mut s = String::new();
         file.read_to_string(&mut s)?;
-        let j: JobInfo<Self::Output, Self::Error> = serde_json::from_str(&s)?;
+        let j: JobInfo<
+            Self::Output,
+            Self::Error,
+            Self::Metadata,
+            Self::Status,
+        > = serde_json::from_str(&s)?;
         Ok(j)
     }
 }
--- tests/fs_job.rs
+++ tests/fs_job.rs
@@ -1,24 +1,52 @@
 use std::time::Duration;
 
 use serde::{Deserialize, Serialize};
-use simple_jobs::{fs_job::FSJob, Job, JobStatus};
+use simple_jobs::{fs_job::FSJob, Job, StatusType};
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
 struct MyError {}
 
+#[derive(Clone, Debug, Default, Serialize, Deserialize)]
+struct MyMetadata {
+    value: usize,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct MyStatus {
+    value: u16,
+}
+
+impl StatusType for MyStatus {
+    type Base = u16;
+
+    fn started() -> Self {
+        MyStatus { value: 0 }
+    }
+
+    fn finished() -> Self {
+        MyStatus { value: 1 }
+    }
+
+    fn status(value: Self::Base) -> Self {
+        MyStatus { value }
+    }
+}
+
 #[tokio::test]
 async fn test_submit() -> std::io::Result<()> {
     let dir = tempfile::tempdir()?;
-    let job: FSJob<u16, MyError> = FSJob::new(dir.path().into());
-    let j = job.submit(|_id, _job| async move { Ok(1u16) })?;
+    let metadata = Default::default();
+    let job: FSJob<u16, MyError, MyMetadata, MyStatus> =
+        FSJob::new(dir.path().into());
+    let j = job.submit(|_id, _job, _| async move { Ok(1u16) }, metadata)?;
     let j2 = loop {
         let jj = job.load(j)?;
-        if jj.status == JobStatus::Finished {
+        if jj.status.value == 1 {
             break jj;
         }
         tokio::time::sleep(Duration::from_millis(10)).await;
     };
-    assert_eq!(j2.status, JobStatus::Finished);
+    assert_eq!(j2.status.value, 1);
     assert_eq!(j2.result.unwrap().unwrap(), 1u16);
     Ok(())
 }
